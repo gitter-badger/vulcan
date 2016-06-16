d8d1e013a5de1998692ac43be72c6de863986f20
Josh Manders <josh@joshmanders.com>
2016-06-16 15:20:02 -0500

    dont destructure error to just message

--- app/hooks/errors.js
+++ app/hooks/errors.js
@@ -2,8 +2,8 @@ import errorHandler from 'core/middleware/error-handler'
 import { NotFoundError, BadRequestError } from 'core/errors'
 
 export default function () {
-  this.use(errorHandler(({ message }) => {
-    if (message === 'EmptyResponse') {
+  this.use(errorHandler((e) => {
+    if (e.message === 'EmptyResponse') {
       return new NotFoundError()
     }
     return new BadRequestError()
