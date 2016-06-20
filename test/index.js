8fa984be5230b9c4bdc8a7066e9224099ddb80c5
Josh Manders <josh@joshmanders.com>
2016-06-20 16:52:23 -0500

    add more test coverage for app

--- test/index.js
+++ test/index.js
@@ -7,16 +7,16 @@ import server from 'core/server'
 let app = null
 
 describe('Vulcan Application', () => {
-  describe('GET /', () => {
-    before(() => {
-      return db.migrate.latest()
-        .then(() => db.seed.run())
-        .then(() => {
-          app = request(server.listen())
-        })
-    })
+  before(() => {
+    return db.migrate.latest()
+      .then(() => db.seed.run())
+      .then(() => {
+        app = request(server.listen())
+      })
+  })
 
-    it(`should return "Hello World"`, (done) => {
+  describe('GET /', () => {
+    it('should return Hello World', (done) => {
       app.get('/')
         .expect('Content-Type', /text\/html/)
         .expect(200)
@@ -30,11 +30,7 @@ describe('Vulcan Application', () => {
   })
 
   describe('GET /users', () => {
-    before(() => {
-      app = request(server.listen())
-    })
-
-    it(`should return two users`, (done) => {
+    it('should return two users', (done) => {
       app.get('/users')
         .expect('Content-Type', /json/)
         .expect(200)
@@ -44,6 +40,58 @@ describe('Vulcan Application', () => {
           expect(res.body.data.users).to.not.be.empty
           expect(res.body.data.users).to.have.lengthOf(2)
           done()
+        })
+    })
+  })
+
+  describe('POST /users', () => {
+    it('should create a user', () => {
+      app.post('/users')
+        .send({ email: 'test@test.com', password: 'test' })
+        .expect(200)
+        .end((err, res) => {
+          expect(err).to.be.null
+          expect(res.data.user).to.not.be.empty
+          expect(res.data.user.email).to.equal('test@test.com')
+        })
+    })
+  })
+
+  describe('GET /users/1', () => {
+    it('should return the user with id #1', () => {
+      app.get('/users/1')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, res) => {
+          expect(err).to.be.null
+          expect(res.body.error).to.be.undefined
+          expect(res.body.data.user).to.be.an('object')
+          expect(res.bodoy.data.users.email).to.not.be.empty
+        })
+    })
+  })
+
+  describe('PUT /users/3', () => {
+    it('should update user #3', () => {
+      app.put('/users/3')
+        .send({ email: 'test@example.com' })
+        .expect(200)
+        .end((err, res) => {
+          expect(err).to.be.null
+          expect(res.body.error).to.be.undefined
+          expect(res.body.data.user).to.be.an('object')
+          expect(res.body.data.user.email).to.equal('test@example.com')
+        })
+    })
+  })
+
+  describe('DELETE /users/3', () => {
+    it('should delete user #3', () => {
+      app.delete('/users/3')
+        .expect(200)
+        .end((err, res) => {
+          expect(err).to.be.null
+          expect(res.body.data).to.be.empty()
         })
     })
   })
