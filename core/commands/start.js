6279d84c9ec263d7a0601fa9abef461a817cfa9e
Josh Manders <josh@joshmanders.com>
2016-06-22 22:10:09 -0500

    add pretty start view

--- core/commands/start.js
+++ core/commands/start.js
@@ -1,17 +1,40 @@
+import { repeat } from 'lodash'
+import chalk from 'chalk'
 import env from '@niftyco/env'
 import config from 'core/config'
-import server from 'core/server'
+import { logo } from 'core/vulcan'
+import app from 'core/server'
 
 export const command = 'start'
 export const description = 'start your app'
 export const options = [{
   flags: '-p, --port [int]',
   description: 'the port the app will be running on',
-  default: env.get('port', 1337)
+  default: env.get('port', config.get('app.port', 1337))
+}, {
+  flags: '-B, --no-banner',
+  description: "don't show the banner"
 }]
 
-export const action = (done) => ({ port }) => {
-  server.listen(port, () => {
-    console.log(`${config.get('app.name', 'vulcan-app')} running on port ${port}`)
+export const action = (done) => ({ port, banner }) => {
+  const timeout = 30000
+  const server = app.listen(port, () => {
+    if (banner) {
+      console.log(logo)
+      console.log(`  ${repeat('-', 80)}\n`)
+    }
+    console.log(`  Started ${chalk.gray(config.get('app.name', 'your app'))} in ${chalk.cyan(env.get('node_env', 'development'))} mode at ${chalk.yellow(process.cwd())}`)
+    console.log(`  You can view it by going to ${chalk.blue(`http://localhost:${port}`)} in your browser.`)
+    console.log(`  Press ${chalk.cyan('<CTRL> + <C>')} at any time to shut down Vulcan server.\n`)
+  })
+
+  process.on('SIGINT', () => {
+    console.log(`\n  ${chalk.red('Shutting down server.')}\n`)
+    server.close((e) => process.exit(1))
+
+    setTimeout(() => {
+      console.log(`  ${chalk.red(`Could not close server in time (${timeout}ms), forcefully shutting down.`)}\n`)
+      process.exit(1)
+    }, timeout).unref()
   })
 }
--- core/vulcan.js
+++ core/vulcan.js
@@ -1,3 +1,18 @@
+import { pad } from 'lodash'
+import chalk from 'chalk'
+
 export const version = '0.1.0-alpha.1'
-export const url = 'https://github.com/aniftyco/vulcan'
-export const author = 'NiftyCo <support@anifty.co> (https://anifty.co)'
+export const logo = `
+        ${chalk.red('▐▓')}
+        ${chalk.red('▓▓▓▄')}
+       ${chalk.red('▐▌ ▀▌')}
+       ${chalk.red('▐▌')}
+  █▓▓▓ ${chalk.red('▐')}  ▐▓▓▀   ▓▓▓▀     ▓▓▀   ▓▓▓        ▄▓▓█▀▀▀▀▓▓      ▓▓▓▄     ▐█▓▓▓     ▐▓▓▓
+   █▓▓    ▄▓▓    ▐▓▓      ▐▓    ▓▓▌       ▓▓▓       ▀     ▓▓ ▓▓      ▐▓█▓▓▌    ▓▌
+    ▓▓▓  ▐▓▓      ▓▓      ▐▓    ▓▓▌       ▓▓▄            ▄▓  ▀▓▓     ▐▓  █▓▓▄  ▓▌
+     ▓▓▌ ▓▓       ▓▓      ▐▓    ▓▓▌       ▓▓▓           ▄▓▀▀▀▀▓▓▓    ▐▓    █▓▓▄▓▌
+      ▓▓▓▓        ▓▓▓▄   ▄▓▀    ▓▓▓    ▄▓  ▓▓▓▄    ▄▓  ▄▓      ▓▓▌   ▓▓      ▓▓▓▌
+      ▐██          ▀█████▀     ▀▀▀▀▀▀▀▀▀     ▀▀███▀▀  ▀▀▀▀    ▀▀▀▀▀ ▀▀▀▀      ▀▀▀
+
+  ==[ ${chalk.blue('git.io/vulcan')} ]=${pad(`=[ By ${chalk.magenta('NiftyCo')} (${chalk.blue('anifty.co')}) ]=`, 80 - (version.length + 8), '=')}=[ ${chalk.green(`v${version}`)} ]==
+`
